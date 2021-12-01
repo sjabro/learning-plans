c691e69d8f8f32c8d472a2daf7ad7e83f1a0fe7c
sjabro <resting_wheeze@outlook.com>
2021-11-30 20:11:35 -0700

    Reset to original

--- terraform/scripts/tf_paths.sh
+++ terraform/scripts/tf_paths.sh
@@ -1,11 +1,6 @@
 statePath="<%=state.stateList[0].statePath%>"
-echo "State Path: $statePath"
-
 planPath="<%=state.stateList[0].planPath%>"
-echo "Plan Path: $planPath"
-
 contentPath="<%=state.stateList[0].contentPath%>"
-echo "Content Path: $contentPath"
-
 workingPath="<%=state.stateList[0].workingPath%>"
-echo "Working Path: $workingPath"
+
+echo "StatePath=$statePath, PlanPath=$planPath, ContentPath=$contentPath, Working Path=$workingPath"
