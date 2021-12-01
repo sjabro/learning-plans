04452ea5e1be12d76b9ad21262387637ce658100
sjabro <resting_wheeze@outlook.com>
2021-12-01 09:10:57 -0700

    Fixed syntax

--- terraform/scripts/state_path_result.sh
+++ terraform/scripts/state_path_result.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-statePath="<%=results.tfClass_get_tf_paths%>"
+statePath="<%=results.tfClass_get_tf_paths.StatePath%>"
 
-echo "The state path for this instance is: $statPath"
+echo "The state path for this instance is: $statePath"
