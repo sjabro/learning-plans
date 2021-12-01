726518f87cdd4204a13a22d283ff77d716d12898
sjabro <resting_wheeze@outlook.com>
2021-11-30 18:44:53 -0700

    added path echo

---
+++ terraform/scripts/tf_paths.sh
@@ -0,0 +1,6 @@
+statePath="<%=state.stateList[0].statePath%>"
+planPath="<%=state.stateList[0].planPath%>"
+contentPath="<%=state.stateList[0].contentPath%>"
+workingPath="<%=state.stateList[0].workingPath%>"
+
+echo "StatePath=$statePath, PlanPath=$planPath, ContentPath=$contentPath, Working Path=$workingPath"
